16b2e50f6c36d0ff65e880c18e42c81d46570f84
Dario Manesku <dariomanesku@gmail.com>
2014-05-15 22:27:06 +0200

    Fixed VS2008_DEVENV_DIR.

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@
 # License: http://www.opensource.org/licenses/BSD-2-Clause
 #
 
-VS2008_DEVENV_DIR=C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE
+VS2008_DEVENV_DIR=C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE
 VS2010_DEVENV_DIR=C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE
 VS2012_DEVENV_DIR=C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE
